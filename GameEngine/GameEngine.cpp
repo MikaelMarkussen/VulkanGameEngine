12e276bdd385ce7df6efe23b9c5c8562bce78dda
MikaelMarkussen <244604@stud.inn.no>
2023-04-03 21:24:40 +0200

    first test

--- GameEngine/GameEngine.cpp
+++ GameEngine/GameEngine.cpp
@@ -1,12 +1,30 @@
 ﻿// GameEngine.cpp : Defines the entry point for the application.
 //
 
-#include "GameEngine.h"
-
-using namespace std;
+#include <iostream>
+#include <vulkan/vulkan.h>
+#include "GLFW/glfw3.h"
+#include "glm/glm.hpp"
 
 int main()
 {
-	cout << "Hello CMake." << endl;
-	return 0;
+	glfwInit();
+
+	if (glfwVulkanSupported()) 
+	{
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		GLFWwindow* window = glfwCreateWindow(800, 600, "Mikael's GameEngine", nullptr, nullptr);
+		uint32_t extensionCount = 0;
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+		std::cout << extensionCount << " Extension supportet\n";
+		
+		while (!glfwWindowShouldClose(window))
+		{
+			glfwPollEvents();
+		}
+		glfwDestroyWindow(window);
+		glfwTerminate();
+	}
+	else
+		return 0;
 }
